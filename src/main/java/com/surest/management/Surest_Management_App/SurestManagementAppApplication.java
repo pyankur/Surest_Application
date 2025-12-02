05a1978706866e2481d6afcd0f76a257f0dce205
pyankur <40889261+pyankur@users.noreply.github.com>
2025-12-02 12:51:20 +0530

    Update SurestManagementAppApplication.java

--- src/main/java/com/surest/management/Surest_Management_App/SurestManagementAppApplication.java
+++ src/main/java/com/surest/management/Surest_Management_App/SurestManagementAppApplication.java
@@ -2,8 +2,10 @@ package com.surest.management.Surest_Management_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SurestManagementAppApplication {
 
 	public static void main(String[] args) {
